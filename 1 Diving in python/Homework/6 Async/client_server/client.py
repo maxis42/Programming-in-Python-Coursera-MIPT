05be18db69eeef470566485bba6eb7fc433709b3
Maxim Uvarov <muvarov@ozon.ru>
2020-02-26 10:21:22 +0300

    client almost done

--- 1 Diving in python/Homework/6 Async/client_server/client.py
+++ 1 Diving in python/Homework/6 Async/client_server/client.py
@@ -1,5 +1,6 @@
 import socket
 import time
+import re
 
 
 class Client:
@@ -12,8 +13,12 @@ class Client:
     @property
     def _sock(self):
         socket.setdefaulttimeout(self.timeout)
+        # sock = socket.socket()
+        # sock.connect((self.host, self.port))
         sock = socket.create_connection((self.host, self.port), self.timeout)
-        sock.settimeout(self.timeout)
+        # sock.settimeout(self.timeout)
+        # sock.listen(1)
+        # sock.listen(socket.SOMAXCONN)
         return sock
 
     def get(self, metric_type):
@@ -33,7 +38,8 @@ class Client:
         if timestamp is None:
             timestamp = int(time.time())
 
-        query = " ".join(["put", metric_type, metric_value, timestamp])
+        query = " ".join(["put", str(metric_type), str(metric_value),
+                          str(timestamp)])
         query += "\n"
 
         self.sock.sendall(query.encode("utf-8"))
@@ -46,17 +52,62 @@ class Client:
         self.sock.close()
 
     def _get_response(self):
-        response = ""
-        while True:
-            data = self.sock.recv(1024)
-            if not data:
-                break
-            response += data.decode("utf-8")
-
+        data = self.recvall(self.sock)
+        response = data.decode("utf-8")
+        self._check_response(response)
         response_data = response.split()
         status = response_data[0]
         data = response_data[1:]
         return status, data
+
+    @staticmethod
+    def _check_response(s):
+        data = s.split("\n")
+        status = data[0]
+        if status not in {"ok", "error"}:
+            raise ClientError()
+
+        if (data[-1] != "") or (data[-2] != ""):
+            raise ClientError()
+
+        content = data[1:-2]
+
+        if (status == "error") and (content != "wrong command"):
+            raise ClientError()
+
+        for metric in content:
+            metric_data = metric.split(" ")
+            if len(metric_data) != 3:
+                raise ClientError()
+
+            metric_type, metric_value, timestamp = metric_data
+
+            if re.fullmatch("\w+\.\w+", metric_type) is None:
+                raise ClientError()
+
+            if re.fullmatch("[-+]?\d*\.\d+|\d+", metric_value) is None:
+                raise ClientError()
+
+            if re.fullmatch("\d+", timestamp) is None:
+                raise ClientError()
+
+    # @staticmethod
+    # def fullmatch(regex, string, flags=0):
+    # PYTHON 2
+    #     """Emulate python-3.4 re.fullmatch()."""
+    #     return re.match("(?:" + regex + r")\Z", string, flags=flags)
+
+    @staticmethod
+    def recvall(sock):
+        BUFF_SIZE = 4096  # 4 KiB
+        data = b''
+        while True:
+            part = sock.recv(BUFF_SIZE)
+            data += part
+            if len(part) < BUFF_SIZE:
+                # either 0 or end of data
+                break
+        return data
 
     @staticmethod
     def _parse_get_data(data):
@@ -81,3 +132,16 @@ class Client:
 
 class ClientError(Exception):
     pass
+
+
+if __name__ == "__main__":
+    client = Client("127.0.0.1", 8888, timeout=5)
+
+    client.put("palm.cpu", 0.5, timestamp=1150864247)
+    client.put("palm.cpu", 2.0, timestamp=1150864248)
+    client.put("palm.cpu", 0.5, timestamp=1150864248)
+    client.put("eardrum.cpu", 3, timestamp=1150864250)
+    client.put("eardrum.cpu", 4, timestamp=1150864251)
+    client.put("eardrum.memory", 4200000)
+
+    print(client.get("*"))
